e71af55b3862c4930522dab4b9a2ea7e6f3ac399
mramalingam2203 <dr.muthukumaran@live.com>
2026-07-15 08:49:30 +0530

    user choice add

--- main.go
+++ main.go
@@ -2,20 +2,46 @@ package main
 
 import "fmt"
 
-const appName = "Personal Expense Tracker"
-const MAX_EXPENSES = 10
+const {
+	appName = "Personal Expense Tracker"
+	const MAX_EXPENSES = 10
+}
 
 func main() {
 
 	var categories [MAX_EXPENSES]string
 	var amounts [MAX_EXPENSES]float64
-	var count int = 0
+	var count int
 
-	fmt.Println("==================================")
-	fmt.Println(appName)
-	fmt.Println("==================================")
+	var choice int
 
-	fmt.Println()
+
+ 	ApplicationLoop:
+		for{
+
+			fmt.Println("==================================")
+			fmt.Println(appName)
+			fmt.Println("==================================")
+
+			fmt.Println("1. Add Expense")
+			fmt.Println("2. List Expenses")
+			fmt.Println("3. Show Total")
+			fmt.Println("0. Exit")
+			fmt.Println()
+
+			fmt.Println("Enter your choice : ")
+			fmt.Scanln(&choice)
+switch choice
+{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+}
+
+		}
+
+
 
 	fmt.Println("Today's Expense")
 	fmt.Println()
@@ -32,7 +58,7 @@ func main() {
 
 		fmt.Print("Add another expense? (Y/N): ")
 		fmt.Scanln(&choice)
-		
+
 		if choice == "N" || choice == "n"{
 			break
 		}
