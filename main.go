edff8a3de051b78cd6adad2817ff9079bbf2ad76
mramalingam2203 <dr.muthukumaran@live.com>
2026-07-15 07:43:40 +0530

    array added

--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import "fmt"
 
+const appName = "Personal Expense Tracker"
+const MAX_EXPENSES = 10
+
 func main() {
+
+	var categories [MAX_EXPENSES]string
+	var amounts [MAX_EXPENSES]float64
+	var count int
+
+	categories[0] = "Food"
+	amount[0] = 100.00
+
 	fmt.Println("==================================")
-	fmt.Println(" Personal Expense Tracker")
+	fmt.Println(appName)
 	fmt.Println("==================================")
+
+	fmt.Println()
+
+	fmt.Println("Today's Expense")
+	fmt.Println()
+/*
+	fmt.Print("Enter Expense Category :")
+	fmt.Scanln(&category)
+
+	fmt.Print("Enter Expense Amount   :")
+	fmt.Scanln(&amount)
+	fmt.Println()
+*/
+	fmt.Println("Category :", category[0])
+	fmt.Println("Amount   :", amount[0])
+
 	fmt.Println("Project Started Successfully!")
 }
