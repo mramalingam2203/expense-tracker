084bc906198e6ae615b740bf6cf347b6474e5d90
mramalingam2203 <dr.muthukumaran@live.com>
2026-07-15 07:36:19 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("==================================")
+	fmt.Println(" Personal Expense Tracker")
+	fmt.Println("==================================")
+	fmt.Println("Project Started Successfully!")
+}
